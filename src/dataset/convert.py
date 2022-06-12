322c3f3d1cbc0e54674673982b43bbaf7bd942c2
Ammar Vora <ammarmust4@gmail.com>
2022-06-12 00:05:40 -0400

    added convert script from  annotations in .mat to yolov4 format

--- src/dataset/convert.py
+++ src/dataset/convert.py
@@ -1,10 +1,10 @@
+from cProfile import label
 import scipy.io as scipy
 import numpy as np
 import cv2
+import os
 
-
-# print(list(mat))
-# print(mat["boxes"])
+DATA_DIR = "C:/Users/ammar/Documents/CodingProjects/HandGestures/src/dataset"
 
 
 def get_old_format(mat) -> list:
@@ -14,29 +14,77 @@ def get_old_format(mat) -> list:
         # print(f"This is {i=} {box}\n")
         for j, values in enumerate(box[0]):
             # print(f"Onto {j=} {values}")
+
             boxes.append(
                 [
                     np.int32(values[0][0]),
-                    np.int32(values[2][0]),
+                    np.int32(values[1][0]),
                     np.int32(values[2][0]),
                     np.int32(values[3][0]),
-                    np.int32(values[1][0]),
-                    values[4][0],
                 ]
             )
     return boxes
 
 
-if __name__ == "__main__":
-    mat = scipy.loadmat("test_dataset/test_data/annotations/VOC2007_1.mat")
-    old_format = np.array(get_old_format(mat), dtype=object)
-    print(f"{old_format=}")
+def show_labels(img, x_pts, y_pts):
+    pts = np.column_stack([x_pts, y_pts])
+    print(pts)
 
-    img = cv2.imread("test_dataset/test_data/images/VOC2007_1.jpg")
-    print("trying this ", (old_format[0][:4]))
+    # pts = pts.reshape((-1, 1, 2))
+    print(pts)
     img = cv2.polylines(
-        img, pts=old_format[0][:4], isClosed=True, color=255, thickness=2
+        img,
+        pts=[pts],
+        isClosed=True,
+        color=(255, 0, 0),
+        thickness=2,
     )
 
-    cv2.imshow("test", img)
-    cv2.waitKey(0)
+    return img
+
+
+if __name__ == "__main__":
+    for folder in os.listdir(DATA_DIR):
+        if folder == "convert.py":
+            continue
+        label_path = f"{DATA_DIR}/{folder}/annotations"
+
+        for old_labels in os.listdir(label_path):
+            print(f"{label_path}/{old_labels}")
+            mat = scipy.loadmat(f"{label_path}/{old_labels}")
+            old_format_boxes = np.array(get_old_format(mat), dtype=object)
+
+            img = cv2.imread(
+                f"{DATA_DIR}/{folder}/images/{old_labels.split('.')[0]}.jpg"
+            )
+            h, w, _ = img.shape
+
+            try:
+                os.mkdir(f"{DATA_DIR}/{folder}/labels")
+            except:
+                pass
+            filename = f"{old_labels.split('.')[0]}.txt"
+            file = open(f"{DATA_DIR}/{folder}/labels/{filename}", "w")
+
+            for box in old_format_boxes:
+
+                obj_class = 0
+
+                x_pts = np.array([box[0][1], box[1][1], box[2][1], box[3][1]])
+                x1, x2 = np.min(x_pts), np.max(x_pts)
+
+                y_pts = np.array([box[0][0], box[1][0], box[2][0], box[3][0]])
+                y1, y2 = np.min(y_pts), np.max(y_pts)
+
+                width = float(x2 - x1) / w
+                height = float(y2 - y1) / h
+                X = float(width + x1) / w
+                Y = float(height + y1) / h
+
+                # img = show_labels(img, x_pts, y_pts)
+
+                file.write(f"{obj_class} {X} {Y} {width} {height}\n")
+            file.close()
+
+            # cv2.imshow("test", img)
+            # cv2.waitKey(0)
